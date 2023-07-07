943cec74716c92b2517801a8135f6ea1c566cbee
raman <raman@Lenovo-PC.lan>
2023-07-07 16:55:55 -0400

    Perf Changes

--- DsAlgoTest/src/test/java/team/ninja/ds/algo/hooks/Hooks.java
+++ DsAlgoTest/src/test/java/team/ninja/ds/algo/hooks/Hooks.java
@@ -10,55 +10,45 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
 import io.cucumber.java.Scenario;
 import team.ninja.ds.algo.driver.factory.DriverFactory;
 import team.ninja.ds.algo.utilities.ConfigReader;
 import team.ninja.ds.algo.utilities.LoggerLoad;
 import static java.util.Objects.isNull;
 public class Hooks {
-	private DriverFactory driverFactory;
-	private WebDriver driver;
-	private ConfigReader configReader;
-	Properties prop;
+	private static DriverFactory driverFactory;
+	private static WebDriver driver;
+	private static ConfigReader configReader;
+	static Properties prop;
 	static WebDriverWait wait ;
 	
 	
-	@Before(order=0)//will execute 0 and then 1
-	public void getProperty() throws IOException
+	@BeforeAll(order=0)//will execute 0 and then 1
+	public static void beforeAll() throws IOException
 	{   
 		LoggerLoad.info("Loading Config Properties ");
 		configReader =new ConfigReader();
 		prop=configReader.init_prop();		
-	}
-	
-	@Before(order=1)
-	public void launchBrowser()
-	{
-	    driver = driverFactory.getDriver();
+	    driver = DriverFactory.getDriver();
 	    if(isNull(driver)) {
 			String browserName=prop.getProperty("browser");
 			String geturl=prop.getProperty("url");
 			LoggerLoad.info("Initializing the DriverFactory class ");
-			driverFactory=new DriverFactory();
 		    LoggerLoad.info(browserName+ " browser is Launching");
-			driver=driverFactory.init_driver(browserName,geturl);	
+			driver=DriverFactory.init_driver(browserName,geturl);	
 	    }
-		
 	}
 	
-	@After(order=0)// will execute after "1" then order 0
-	public void quitBrowser()
+	@AfterAll(order=0)// will execute after "1" then order 0
+	public static void afterAll()
 	{
 		LoggerLoad.info("Closing Browser");
-		//driver.quit();
+		driver.quit();
 	}
 
-	@AfterSuite(alwaysRun = true)// will execute after "1" then order 0
-	public void afterAllTestDone()
-	{
-		LoggerLoad.info("afterAllTestDone");
-	}
 
 	@After(order=1)//for After it will start from 1 and then 0
 	public void tearDown(Scenario scenario)
