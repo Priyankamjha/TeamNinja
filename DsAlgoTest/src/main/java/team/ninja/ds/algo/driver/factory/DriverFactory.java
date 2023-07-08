e8471bf0d5fa340b06a305dafed748c4ef0396ba
priyanka <priyanka@priyankas-mbp.lan>
2023-07-08 17:48:38 -0400

    Driver headless

--- DsAlgoTest/src/main/java/team/ninja/ds/algo/driver/factory/DriverFactory.java
+++ DsAlgoTest/src/main/java/team/ninja/ds/algo/driver/factory/DriverFactory.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -20,7 +21,10 @@ public class DriverFactory {
 
 		if (browser.equals("chrome")) {
 			WebDriverManager.chromedriver().setup();
-			tlDriver.set(new ChromeDriver());
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless");
+			ChromeDriver chromeDriver = new ChromeDriver(options);
+			tlDriver.set(chromeDriver);
 		} else if (browser.equalsIgnoreCase("edge")) {
 			WebDriverManager.edgedriver().setup();
 			tlDriver.set(new EdgeDriver());
